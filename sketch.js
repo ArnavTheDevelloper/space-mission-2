51ca18172cf3e9b0deb8148b989fea7bd7d32fce
ArnavTheDevelloper <64299098+ArnavTheDevelloper@users.noreply.github.com>
2020-05-18 20:25:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground1
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground1 = new Ground (370,650,150,20);
+	ground2 = new Ground (300,610,20,100);
+	ground3 = new Ground (440,610,20,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  if (keyPressed(DOWN_ARROW)){
+680
+packageBody.velocityY=-1;
+packageBody.add(gravity);
+
+var options = {
+
+packageBody : restitution=100
+
+}
+
+  }
+  drawSprites();
+  ground1.display();
+  ground2.display();
+  ground3.display();
+ 
+}
+
+
+function gravity(){
+if (keyPressed(DOWN_ARROW)){
+
+	packageBody.velocityY=-1;
+	
+	}
+}
+	
+function keyPressed() {
+	if (keyCode === DOWN_ARROW) {
+	   Matter.Body.setStatic(packageBody,false);
+	   
+	 }
+   }
+	   
